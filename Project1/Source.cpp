6e60a34a8f137225e3a963d8cff35d063e3feee2
brandonsmithccc <smith.brandon7@student.clackamas.edu>
2022-09-27 08:21:21 -0700

    added user input

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
-	int x = 4;
-	x = x - 2;
-	cout << "My value of x is " << x << endl;
+	int x;
+	cout << "Enter a value for x: ";
+	cin >> x;
+	x = x * 2;
+	cout << "The doubled value of x is " << x << endl;
 	
 	return 1;
 }
